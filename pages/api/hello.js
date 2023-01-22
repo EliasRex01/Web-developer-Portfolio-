d79da320552c65003bb82013ecba3a50fe0bb137
Rex Elias <56458391+Misael08@users.noreply.github.com>
2023-01-22 20:22:38 -0300

    Update hello.js

---
+++ pages/api/hello.js
@@ -0,0 +1,6 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({ name: 'John Doe' })
+}
+//la api se carga desde next js
